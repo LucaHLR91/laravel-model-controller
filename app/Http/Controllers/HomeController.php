be70ad5a410a150f363eec22622e2f82835152d9
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-10-28 16:39:56 +0200

    HomeController route

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use app\Movie;
 
 class HomeController extends Controller
 {
-    //
+    public function home() {
+        return view('home');
+    }
 }
