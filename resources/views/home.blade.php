61bf6e9b1c9f8adfc8a91432e9a3461ac5f9df06
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-10-28 16:58:52 +0200

    movies list

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,17 @@
 @extends('layout.app')
 
 @section('title', 'Home')
+
+@section('movies')
+    <main>
+        <h1 class="main_title">I nostri film</h1>
+
+        <div class="movies-list">
+            <ul>
+                @foreach ($movies as $index => $movie)
+                    <li>{{ $movie['title'] }}</li>
+                @endforeach
+            </ul>
+        </div>
+    </main>
+@endsection
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -8,6 +8,7 @@
 </head>
 <body>
     @include('partials.header')
+    @yield('movies')
     @include('partials.footer')
 </body>
 </html>
